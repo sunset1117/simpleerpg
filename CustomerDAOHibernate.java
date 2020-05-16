227f4ac96d4da1811da20c252189a8ac1e934211
sunset1117 <sunset1117@hotmail.com>
2020-05-16 15:04:45 +0800

    version-1.0

---
+++ CustomerDAOHibernate.java
@@ -0,0 +1,46 @@
+package model.dao;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+
+import model.CustomerBean;
+import model.CustomerDAO;
+import model.hibernate.HibernateUtil;
+
+public class CustomerDAOHibernate implements CustomerDAO {
+	private Session session;
+	public CustomerDAOHibernate(Session session) {
+		this.session = session;
+	}	
+	public Session getSession() {
+		return session;
+	}
+	public static void main(String[] args) {
+		HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
+
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		CustomerDAO customerDao = new CustomerDAOHibernate(session);
+		
+		CustomerBean select = customerDao.select("Alex");
+		System.out.println("select="+select);
+		
+		HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().commit();
+	}
+	@Override
+	public CustomerBean select(String custid) {
+		return this.getSession().get(CustomerBean.class, custid);
+	}
+	@Override
+	public boolean update(byte[] password, String email, Date birth, String custid) {
+		CustomerBean temp = 
+				this.getSession().get(CustomerBean.class, custid);
+		if(temp!=null) {
+			temp.setPassword(password);
+			temp.setEmail(email);
+			temp.setBirth(birth);
+			return true;
+		}
+		return false;
+	}
+}
